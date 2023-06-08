f2a90e8953b5d19d3bd8763ef7d740da058fbd3a
paolodeltorto <paolo.deltorto@gmail.com>
2023-06-08 10:45:04 +0200

    bloc

---
+++ lib/blocs/matches/matches_state.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+part of 'matches_bloc.dart';
+
+abstract class MatchesState extends Equatable {
+  const MatchesState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class FetchingMatchesState extends MatchesState {}
+
+class FetchedMatchesState extends MatchesState {
+  final List<Match> matches;
+
+  const FetchedMatchesState(this.matches);
+
+  @override
+  List<Object?> get props => [matches];
+}
+
+class NoMatchesState extends MatchesState {}
+
+class ErrorMatchesState extends MatchesState {
+  final String? errorMessage;
+
+  const ErrorMatchesState([this.errorMessage]);
+
+  @override
+  List<Object?> get props => [errorMessage];
+}
